e4889d1ff6b12ea40dbc69f74ae3227cba5ec576
Artificer 111 <artificeroneeleven@gmail.com>
2024-07-31 18:00:28 -0400

    Fix consistent move code.

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -10,12 +10,16 @@ public class Entity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = new MovementLogic(GetComponent<Rigidbody>(), 10);
+		control = new ControlLogic();
+
+		movement.MoveToDirection(Vector3.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (movement != null)
+			movement.Update(Time.deltaTime);
     }
 }
--- Assets/Scripts/Entities/MovementLogic.cs
+++ Assets/Scripts/Entities/MovementLogic.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementLogic
 {
-    
+	private Rigidbody rb;
+	private Vector3 targetDirection = Vector3.zero;
+	private float speed = 10;
+
+	public MovementLogic(Rigidbody rb, float speed)
+	{
+		this.rb = rb;
+		this.speed = speed;
+	}
+
+	// Change movement direction.
+	public void MoveToDirection(Vector3 direction)
+	{
+		targetDirection = direction;
+	}
+
+    public void Update(float deltaTime)
+	{
+		if (rb == null)
+			return;
+		
+		rb.AddForce(targetDirection * speed * deltaTime, ForceMode.VelocityChange);
+	}
 }
