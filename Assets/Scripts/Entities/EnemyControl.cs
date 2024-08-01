66176116a1887b295b710fff000b122965ae0877
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-01 19:31:19 -0400

    Basic AI that runs at you

---
+++ Assets/Scripts/Entities/EnemyControl.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class EnemyControl : MonoBehaviour
+{
+	private Vector3 moveDirection = Vector2.zero;
+	private Entity entity;
+	private Vector3 aimPosition = Vector3.zero;
+	private Entity targetPlayer;
+
+	private void Awake()
+	{
+		entity = GetComponent<Entity>();
+	}
+
+	/// <summary>
+	/// Update the position of the targeting cursor as well as the hovered target.
+	/// </summary>
+	/// <returns>
+	/// Returns the target GameObject which was hovered.
+	/// </returns>
+	private GameObject TargetCursor()
+	{
+		if (targetPlayer == null)
+		{
+			GameObject potentialPlayer = GameObject.FindWithTag("Player");
+			if (potentialPlayer != null)
+				targetPlayer = potentialPlayer.GetComponent<Entity>();
+		}
+		else if (targetPlayer.IsDead)
+		{
+			targetPlayer = null;
+		}
+		else
+		{
+			moveDirection = new Vector3(targetPlayer.transform.position.x - transform.position.x, 0, targetPlayer.transform.position.z - transform.position.z);
+			aimPosition = targetPlayer.transform.position;
+			return targetPlayer.gameObject;
+		}
+
+		return null;
+	}
+
+	private void Update()
+	{
+		if (entity == null)
+			return;
+
+		entity.MoveToDirection(new Vector3(moveDirection.x, 0, moveDirection.z));
+		TargetCursor();
+		entity.FacePosition(aimPosition);
+	}
+
+}
