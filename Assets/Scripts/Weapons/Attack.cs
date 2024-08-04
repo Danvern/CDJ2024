64ea71c54e7c4fc34998f7747271080bb9324886
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-03 21:31:44 -0400

    Attack effect framework for adding extra properties to attacks besides generating projectiles

--- Assets/Scripts/Weapons/Attack.cs
+++ Assets/Scripts/Weapons/Attack.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IAttackEffect
+{
+	void Activate();
+
+	void Deactivate();
+}
+
 public class Attack : MonoBehaviour
 {
 	[SerializeField] GameObject projectile;
 	[SerializeField] bool melee = false;
+	IAttackEffect[] effects = new IAttackEffect[0];
 	private ParticleSystem particles;
 	private Entity owner;
 
@@ -24,12 +32,22 @@ public class Attack : MonoBehaviour
 			ProjectileManager.Instance.GenerateProjectile(projectile, transform.position, transform.rotation, transform, owner);
 		else
 			ProjectileManager.Instance.GenerateProjectile(projectile, transform.position, transform.rotation, owner);
+
+		foreach (IAttackEffect effect in effects)
+		{
+			effect.Activate();
+		}
 	}
 
 	public void Deactivate()
 	{
 		if (particles != null)
 			particles.Stop();
+
+		foreach (IAttackEffect effect in effects)
+		{
+			effect.Activate();
+		}
 	}
 
 	void Awake()
