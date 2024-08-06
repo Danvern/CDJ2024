cb1886eacd677dc3a4ca5b8926134ef4dc93395b
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-06 10:14:59 -0400

    GameObject extension

---
+++ Assets/Scripts/Utilities/OwlGameObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public static class OwlGameObject
+{
+	/// <summary>
+	/// Returns the object itself if it exists, null otherwise.
+	/// </summary>
+	/// <typeparam name="T">The type of the object.</typeparam>
+	/// <param name="obj">The object being checked.</param>
+	/// <returns>The object itself if it exists and not destroyed, null otherwise.</returns>
+	public static T OrNull<T>(this T obj) where T : Object => obj ? obj : null;
+}
