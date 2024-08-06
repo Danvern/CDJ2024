36ca4cc98c7b6829f22e10cadc9f7145ff11f718
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-06 09:54:47 -0400

    Update OwlVector with extensions.

--- Assets/Scripts/Utilities/OwlVector.cs
+++ Assets/Scripts/Utilities/OwlVector.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 
-public class OwlVector : MonoBehaviour
+public static class OwlVector
 {
+	/// <summary>
+	/// Sets any values of the Vector3
+	/// </summary>
+	public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+	{
+		return new Vector3(x ?? vector.x, y ?? vector.y, z ?? vector.z);
+	}
+
+	/// <summary>
+	/// Adds to any values of the Vector3
+	/// </summary>
+	public static Vector3 Add(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+	{
+		return new Vector3(vector.x + (x ?? 0), vector.y + (y ?? 0), vector.z + (z ?? 0));
+	}
+
 	public static void LimitRigidBodyVelocity(Rigidbody rb, float maxVelocity)
 	{
 		float overspeed = maxVelocity - rb.velocity.magnitude;
