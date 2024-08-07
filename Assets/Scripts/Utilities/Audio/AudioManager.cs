2ebee0d9e3001e684b5f9ce19b0a422d4e45e220
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-07 15:09:26 -0400

    Music implemented. Redundant audio manager removed.

--- Assets/Scripts/Utilities/Audio/AudioManager.cs
+++ Assets/Scripts/Utilities/Audio/AudioManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+	[field: Header("Music")]
+	[field: SerializeField] public EventReference Music { get; private set; }
 	public static AudioManager Instance { get; private set; }
 	private LinkedList<EventInstance> activeInstances = new();
 	public float MusicVolume { get; set; }
+	private EventInstance musicLoop;
 
 
 	private void Awake()
@@ -17,6 +20,15 @@ public class AudioManager : MonoBehaviour
 			Debug.LogError("!Found more than one AudioManager in the scene.");
 		}
 		Instance = this;
+	}
+
+	private void Start()
+	{
+		if (!Music.IsNull)
+		{
+			musicLoop = CreateInstance(Instance.Music);
+			musicLoop.start();
+		}
 	}
 
 	public void PlayOneShot(EventReference sound, Vector3 origin)
