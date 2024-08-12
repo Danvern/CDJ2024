ec29a5a871ee5f9da117d71cc85a729dd01776ba
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-11 20:59:29 -0400

    Setup controls for mana bar

---
+++ Assets/ManaBar.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityServiceLocator;
+
+public class ManaBar : MonoBehaviour
+{
+	[SerializeField] Image activeBar;
+	Material activeMaterial;
+	float displayValue;
+	float displayValueMax;
+
+	public float UpdateDisplayValue(float value) => displayValue = value;
+	public float UpdateDisplayValueMax(float value) => displayValueMax = value;
+
+	void Awake()
+	{
+		ServiceLocator.Global.Register(this);
+
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		activeMaterial = activeBar.materialForRendering;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+		if (displayValueMax > 0)
+			activeMaterial.SetFloat("_Fullness", displayValue / displayValueMax);
+		else
+			activeMaterial.SetFloat("_Fullness", 0);
+    }	
+
+	int CapIndex() {
+		return Mathf.FloorToInt(displayValue / displayValueMax * 4);
+
+	}
+}
