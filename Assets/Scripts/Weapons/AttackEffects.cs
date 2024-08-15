4c5809b5f4258eca60e5638ce185f47506880c1a
Artificer 111 <artificeroneeleven@gmail.com>
2024-08-14 20:17:10 -0400

    Updated dash effect factory

--- Assets/Scripts/Weapons/AttackEffects.cs
+++ Assets/Scripts/Weapons/AttackEffects.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "DashEffect", menuName = "GameplayDefinitions/AttackEffect/DashEffect", order = 1)]
 public class DashEffectData : ScriptableObject, IAttackEffectData
 {
 	[SerializeField] float power;
 	[SerializeField] float slideTime;
+	[SerializeField] float offBeatPenalty;
+	[SerializeField] float offBeatTimePenalty;
 	[SerializeField] bool controlled = true;
 	[SerializeField] bool invulnerable = false;
 
 	public IAttackEffect CreateEffect()
 	{
-		DashEffect effect = new DashEffect(power, slideTime, controlled, invulnerable);
- 		return effect;
+		DashEffect effect = new DashEffect.Builder()
+		.WithPower(power)
+		.WithSlideTime(slideTime)
+		.WithOffBeatPenalty(offBeatPenalty)
+		.WithOffBeatTimePenalty(offBeatTimePenalty)
+		.WithInvulnerable(invulnerable)
+		.WithControlled(controlled)
+		.Build();
+		return effect;
 	}
 }
 
